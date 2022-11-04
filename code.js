d904f97157baf4f0826e2921958e3a840b67b791
DonFalche <donfalche@yahoo.com>
2022-11-04 12:57:40 -0300

    falta hacer que el codigo repita mientras no se llegue a 5

--- code.js
+++ code.js
@@ -1,7 +1,12 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundCounter = 0;
 
+
+//prompt to get the player choice
 const playerChoide = prompt('rock... paper... scissors!')
 
-
+//function to ge the computer choice
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
     if (randomNumber == 0){
@@ -11,10 +16,9 @@ function getComputerChoice() {
     } else {
         return 'SCISSORS'
     }
-  }
+}  
 
-console.log('computer choice is ',getComputerChoice())  
-
+//function to process the player chice
 function getPlayerChoice(){
     if (playerChoide.toLowerCase() == 'rock'){
         return 'ROCK'
@@ -27,25 +31,43 @@ function getPlayerChoice(){
     }
 }
 
-console.log('player choice is ',getPlayerChoice())
-
+//turn the fucntions results into constants
 const playerHand = getPlayerChoice();
 const computerHand = getComputerChoice();
 
 console.log('player hand is ',playerHand)
 console.log('computer hand is ',computerHand)
 
-function round(playerHand, computerHand){
-    if (playerHand == 'INVALID CHOICE!!! TRY AGAIN.'){
-        console.log('INVALID CHOICE!!! TRY AGAIN.')
-    } else if (playerHand == 'ROCK' && computerHand == 'SCISSORS' || playerHand == 'PAPER' && computerHand == 'ROCK' || playerHand == 'SCISSORS' && computerHand == 'PAPER'){
-        console.log('YAY!!! YOU WIN')
-    } else if(computerHand == 'ROCK' && playerHand == 'SCISSORS' || computerHand == 'PAPER' && playerHand == 'ROCK' || computerHand == 'SCISSORS' && playerHand == 'PAPER'){
-        console.log('TOO BAD, COMPUTER WINS')
-    } else if (computerHand == 'ROCK' && playerHand == 'ROCK' || computerHand == 'PAPER' && playerHand == 'PAPER' || computerHand == 'SCISSORS' && playerHand == 'SCISSORS'){
-        console.log('LETS CALL IT EVEN')
-    }
 
+//function to define the winner of a round
+function round(playerHand, computerHand){
+    let result = '';
+    if (playerHand == 'INVALID CHOICE!!! TRY AGAIN.'){
+        result = ('invalid')
+    } else if (playerHand == 'ROCK' && computerHand == 'SCISSORS' || playerHand == 'PAPER' && computerHand == 'ROCK' || playerHand == 'SCISSORS' && computerHand == 'PAPER'){
+        playerScore ++, roundCounter++;
+        result = ('you win this round!! ' + playerHand + ' beats ' + computerHand + ', ' + 'player score: ' + playerScore + ' computer score: ' + computerScore)
+        
+    } else if(computerHand == 'ROCK' && playerHand == 'SCISSORS' || computerHand == 'PAPER' && playerHand == 'ROCK' || computerHand == 'SCISSORS' && playerHand == 'PAPER'){
+        computerScore++, roundCounter++;
+        result = ('you loose this round ' + computerHand + ' beats ' + playerHand + ', ' + 'player score: ' + playerScore + ' computer score: ' + computerScore)
+        
+    } else if (computerHand == 'ROCK' && playerHand == 'ROCK' || computerHand == 'PAPER' && playerHand == 'PAPER' || computerHand == 'SCISSORS' && playerHand == 'SCISSORS'){
+        roundCounter++;
+        result = ('tie')
+    }
+    return result;
 }
 
+
 console.log(round(playerHand, computerHand));
+console.log(playerScore)
+console.log(computerScore)
+console.log(roundCounter)
+
+
+function game(playerScore, computerScore){
+    while (playerScore < 5 && computerScore < 5){
+        round(playerHand, computerHand)
+    }
+}
