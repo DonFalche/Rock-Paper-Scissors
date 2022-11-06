731cee225c1a9a089bceded4ba445496b5d7d56c
DonFalche <donfalche@yahoo.com>
2022-11-06 16:36:42 -0300

    5 round and score system added

--- code.js
+++ code.js
@@ -5,20 +5,20 @@ function getComputerChoice(){
 }
 
 //turn computer choice into variable
-let computerSelection = getComputerChoice();
+//let computerSelection = getComputerChoice();
 //console.log('computer choice is ',computerSelection);
 
 //determine player selection
-let playerSelection = prompt('rock... paper... scissors...').toLowerCase();
+//let playerSelection = prompt('rock... paper... scissors...').toLowerCase();
 //console.log('player choice is ',playerSelection);
 
 //1 round of the game
-function playRound(){
-    if (playerSelection == 'rock' && computerSelection == 'rock' || playerSelection == 'paper' && computerSelection == 'paper' || playerSelection == 'scissors' && computerSelection == 'scissors'){
+/*function playRound(){
+    if ((playerSelection == 'rock' && computerSelection == 'rock') || (playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors')){
         return ('its a tie! you both picked ' + playerSelection)
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper'){
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
         return ('congratulations!!! ' + playerSelection + ' beats ' + computerSelection + ', you win!!!')
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock' || playerSelection == 'rock' && computerSelection == 'paper' || playerSelection == 'paper' && computerSelection == 'scissors'){
+    } else if ((playerSelection == 'scissors' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors')){
         return ('too bad, ' + computerSelection + ' beats ' + playerSelection + ', you loose')
     } else {
         return ('error ' + playerSelection + ' its not a valid answer')
@@ -26,4 +26,32 @@ function playRound(){
     
 }
 
-console.log(playRound())
+console.log(playRound())*/
+
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        function playRound(){
+            let computerSelection = getComputerChoice();
+            let playerSelection = prompt('rock... paper... scissors...').toLowerCase();
+            console.log('computer selection is ' + computerSelection);
+            console.log('player selection is ' + playerSelection);
+            if ((playerSelection == 'rock' && computerSelection == 'rock') || (playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors')){
+                return ('its a tie! you both picked ' + playerSelection + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+            } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
+                playerScore++; return ('congratulations!!! ' + playerSelection + ' beats ' + computerSelection + ', you win!!!' + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+            } else if ((playerSelection == 'scissors' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors')){
+                computerScore++; return ('too bad, ' + computerSelection + ' beats ' + playerSelection + ', you loose' + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+            } else {
+                return ('error ' + playerSelection + ' its not a valid answer')
+            }
+            
+        }
+        
+        console.log(playRound());
+     }
+     
+}
+
+console.log(game());
