95c24fda49bebd1908623c77d2ffe49ad1fb97ba
DonFalche <donfalche@yahoo.com>
2022-11-06 16:53:48 -0300

    round counter and final result

--- code.js
+++ code.js
@@ -1,57 +1,59 @@
 //determine computer choice
 function getComputerChoice(){
+    //create an array with the tree options
     const pick = ['rock', 'paper', 'scissors'];
+    //use math.random to pick an option from the array
     return pick[Math.floor(Math.random() * pick.length)];
 }
 
-//turn computer choice into variable
-//let computerSelection = getComputerChoice();
-//console.log('computer choice is ',computerSelection);
-
-//determine player selection
-//let playerSelection = prompt('rock... paper... scissors...').toLowerCase();
-//console.log('player choice is ',playerSelection);
-
-//1 round of the game
-/*function playRound(){
-    if ((playerSelection == 'rock' && computerSelection == 'rock') || (playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors')){
-        return ('its a tie! you both picked ' + playerSelection)
-    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
-        return ('congratulations!!! ' + playerSelection + ' beats ' + computerSelection + ', you win!!!')
-    } else if ((playerSelection == 'scissors' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors')){
-        return ('too bad, ' + computerSelection + ' beats ' + playerSelection + ', you loose')
-    } else {
-        return ('error ' + playerSelection + ' its not a valid answer')
-    }
-    
-}
-
-console.log(playRound())*/
-
+//the game
 function game(){
+    //first define player score, computer score and round number
     let playerScore = 0;
     let computerScore = 0;
+    let roundNumber = 0;
+
+    //this is a 5 round game, each round the computer choses a hand and ask the player for his hand and then compare to determine the winer
     for (let i = 0; i < 5; i++) {
         function playRound(){
             let computerSelection = getComputerChoice();
             let playerSelection = prompt('rock... paper... scissors...').toLowerCase();
             console.log('computer selection is ' + computerSelection);
             console.log('player selection is ' + playerSelection);
+            //depending on the player and computer selections, the game adds a round, adds a point to one or the other and delivers a message.
+            //tie 
             if ((playerSelection == 'rock' && computerSelection == 'rock') || (playerSelection == 'paper' && computerSelection == 'paper') || (playerSelection == 'scissors' && computerSelection == 'scissors')){
-                return ('its a tie! you both picked ' + playerSelection + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+                roundNumber ++; return ('its a tie! you both picked ' + playerSelection + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+            //player wins    
             } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') || (playerSelection == 'scissors' && computerSelection == 'paper')){
-                playerScore++; return ('congratulations!!! ' + playerSelection + ' beats ' + computerSelection + ', you win!!!' + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+                roundNumber ++, playerScore++; return ('congratulations!!! ' + playerSelection + ' beats ' + computerSelection + ', you win!!!' + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+            //computer wins    
             } else if ((playerSelection == 'scissors' && computerSelection == 'rock') || (playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors')){
-                computerScore++; return ('too bad, ' + computerSelection + ' beats ' + playerSelection + ', you loose' + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+                roundNumber ++, computerScore++; return ('too bad, ' + computerSelection + ' beats ' + playerSelection + ', you loose' + ' scoreboard is: COMPUTER: ' + computerScore + ', PLAYER: ' + playerScore)
+            //player summons an invalid answer    
             } else {
-                return ('error ' + playerSelection + ' its not a valid answer')
+                roundNumber ++; return ('error ' + playerSelection + ' its not a valid answer')
             }
             
         }
-        
+        //console log the result of each round
         console.log(playRound());
-     }
+    }
+    //when the round number reaches 5 (the final round) the game compares scores and anounces the winner or if it is a tie
+    if (roundNumber == 5){
+        //player wins
+        if (playerScore > computerScore){
+            console.log('CONGRATULATIONS!!!! the final score is PLAYER: ' + playerScore + ' COMPUTER: ' + computerScore + ' YOU WIN!!!!')
+        //computer wins
+        } else if(computerScore > playerScore){
+            console.log('too bad, the final score is PLAYER: ' + playerScore + ' COMPUTER: ' + computerScore + ' computer wins, better luck next time')
+        //tie
+        } else{
+            console.log('the final score is PLAYER: ' + playerScore + ' COMPUTER: ' + computerScore + ' nobody wins')
+        }
+    }
      
 }
 
-console.log(game());
+//call the game function in order to get executed
+game();
